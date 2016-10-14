b65919f8c51cfe389dbdc6f67dfd1ab5c5d63677
Tomas Celizna <tomas.celizna@gmail.com>
2016-10-14 12:41:14 +0200

    html safe

--- lib/hashtags/mongoid_extension.rb
+++ lib/hashtags/mongoid_extension.rb
@@ -22,10 +22,10 @@ Mongoid::Fields.option :hashtags do |cls, field, value|
   field.define_singleton_method :demongoize do |*args|
     res = super(*args)
     res.define_singleton_method :to_markup do
-      Hashtags::Builder.to_markup(res.to_s, options)
+      Hashtags::Builder.to_markup(res.to_s, options).html_safe
     end
     res.define_singleton_method :to_hashtag do
-      Hashtags::Builder.to_hashtag(res.to_s, options)
+      Hashtags::Builder.to_hashtag(res.to_s, options).html_safe
     end
     res
   end
